2b2bd496e351ab7f781e928a8e0d500e331f667c
Nick <ntorgov@gmail.com>
2019-04-25 22:19:18 +0300

    Столбик

--- euler-0020/Program.cs
+++ euler-0020/Program.cs
@@ -6,9 +6,9 @@ namespace euler_0020
 	{
 		static void Main(string[] args)
 		{
-			long Base = 100;
+			long Base = 20;
 
-			long factorial = Factorial(Base);
+			string factorial = Factorial(Base);
 			long result = Sum(factorial);
 
 			Console.WriteLine("Factorial is: " + factorial);
@@ -21,13 +21,13 @@ namespace euler_0020
 		/// </summary>
 		/// <param name="Number"></param>
 		/// <returns></returns>
-		private static long Sum(long Number)
+		private static long Sum(string Number)
 		{
 			long Result = 0;
 
-			string number = Number.ToString();
+			//string number = Number.ToString();
 
-			foreach (char value in number)
+			foreach (char value in Number)
 			{
 				Result = Result + Int64.Parse(value.ToString());
 			}
@@ -40,17 +40,69 @@ namespace euler_0020
 		/// </summary>
 		/// <param name="Number"></param>
 		/// <returns></returns>
-		private static long Factorial(long Number)
+		private static string Factorial(long Number)
 		{
-			long Result = 1;
+			string Result = "1";
 
 			while (Number >= 1)
 			{
-				Result = Result * Number;
+				Result = Multipler(Result, Number.ToString());
 				Number--;
 			}
 
 			return Result;
+		}
+
+		public static string Multipler(string Value1, string Value2)
+		{
+			var maxLength = Math.Max(Value1.Length, Value2.Length);
+			var workLength = maxLength * 2;
+
+			var line1 = new String('0', workLength - Value1.Length);
+			var line2 = new String('0', workLength - Value2.Length);
+			char[] line3 = new char[workLength];
+
+			line1 = line1 + Value1;
+			line2 = line2 + Value2;
+
+			for (var x = workLength - 1; x > 0; x--)
+			{
+				var val1 = int.Parse(line1[x].ToString());
+				var val2 = int.Parse(line2[x].ToString());
+				var currentResult = val1 * val2;
+				var decimals = 0;
+				var digits = currentResult;
+				var currentSegment = 0;
+				// var tempVal = line3[x];
+				if ('\0' != line3[x])
+				{
+					currentSegment = int.Parse(line3[x].ToString());
+				}
+				var nextSegment = 0;
+				if (line3[x - 1] != '\0')
+				{
+					nextSegment = int.Parse(line3[x - 1].ToString());
+				}
+				currentResult = currentResult + currentSegment + (nextSegment * 10);
+				if (currentResult >= 10)
+				{
+					decimals = int.Parse(currentResult.ToString()[0].ToString());
+					digits = int.Parse(currentResult.ToString()[1].ToString());
+
+					line3[x] = digits.ToString()[0];
+					line3[x - 1] = decimals.ToString()[0];
+				}
+				else
+				{
+					line3[x] = currentResult.ToString()[0];
+				}
+
+				line3[x] = digits.ToString()[0];
+			}
+
+			Console.WriteLine(line3.ToString());
+
+			return line3.ToString();
 		}
 	}
 }
