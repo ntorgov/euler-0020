526116ff1c8e18063e499fcef1af80aec75151c6
Nick <ntorgov@gmail.com>
2019-04-25 21:29:54 +0300

    Progress

--- euler-0020/Program.cs
+++ euler-0020/Program.cs
@@ -6,7 +6,51 @@ namespace euler_0020
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			long Base = 100;
+
+			long factorial = Factorial(Base);
+			long result = Sum(factorial);
+
+			Console.WriteLine("Factorial is: " + factorial);
+			Console.WriteLine("Result is: " + result);
+			Console.ReadLine();
+		}
+
+		/// <summary>
+		/// Sum of digits
+		/// </summary>
+		/// <param name="Number"></param>
+		/// <returns></returns>
+		private static long Sum(long Number)
+		{
+			long Result = 0;
+
+			string number = Number.ToString();
+
+			foreach (char value in number)
+			{
+				Result = Result + Int64.Parse(value.ToString());
+			}
+
+			return Result;
+		}
+
+		/// <summary>
+		/// Factorial
+		/// </summary>
+		/// <param name="Number"></param>
+		/// <returns></returns>
+		private static long Factorial(long Number)
+		{
+			long Result = 1;
+
+			while (Number >= 1)
+			{
+				Result = Result * Number;
+				Number--;
+			}
+
+			return Result;
 		}
 	}
 }
